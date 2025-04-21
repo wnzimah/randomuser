85941602192e2822b9e15313223a842916d89735
zimah <wnzimah@gmail.com>
2025-04-21 08:49:57 +0800

    change color

--- lib/randomuserscreen.dart
+++ lib/randomuserscreen.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-
+//zimah
 class RandomUserScreen extends StatefulWidget {
   const RandomUserScreen({super.key});
 
