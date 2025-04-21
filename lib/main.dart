7ae152a4a856580574ab95d14ace35fc69615b79
zimah <wnzimah@gmail.com>
2025-04-21 08:57:50 +0800

    change duration

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 6), () {
+    Future.delayed(const Duration(seconds: 10), () {
       if (mounted) {
         Navigator.pushReplacement(
           context,
